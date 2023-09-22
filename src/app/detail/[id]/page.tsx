4c96c6a638e9f69b92e8ce781af19774bc26948e
HwangHyeJin <h960502hhj@gmail.com>
2023-09-22 23:48:50 +0900

    feat: list 페이지 추가

---
+++ src/app/detail/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { ObjectId } from "mongodb";
+import { connectDB } from "../../../../util/database";
+
+type PageParams = {
+  id: number | ObjectId | Uint8Array;
+};
+
+export default async function Detail({ params }: { params: PageParams }) {
+  const db = (await connectDB).db("forum");
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(params.id) });
+
+  return (
+    <div>
+      <h4>상세페이지임</h4>
+      <h4>{result.title}</h4>
+      <p>{result.content}</p>
+    </div>
+  );
+}
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,23 +1,17 @@
 import { connectDB } from "../../../util/database";
 
 export default async function List() {
-  const client = await connectDB;
-  const db = client.db("forum");
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
 
   return (
     <div className="list-bg">
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
+      {result.map((list) => (
+        <div className="list-item" key={list.id}>
+          <h4>{list.title}</h4>
+          <p>{list.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
